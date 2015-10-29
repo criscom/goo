4eede75d9b16965dce0ebc19cf109a002a125bcb
criscom <criscom27@gmail.com>
2015-10-29 23:43:28 +0100

    PS-16: Fix Safari bug for Feeling Lucky button

--- js/script.js
+++ js/script.js
@@ -11,54 +11,40 @@
 // - https://drupal.org/node/1446420
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
 (function($) {
+    $(window).load(function() {
+        /* BEGIN: Feeling Lucky javascript */
+        if (!Modernizr.touchevents) { 
+            $( '<input type="submit" id="edit-submit-2" name="op" value="I\'m feeling lucky!" class="btn-primary btn form-submit"><div class="shuffler" role="button"><div class="pattern"><a href="http://www.paperwhite-studio.com/work/" target="_blank"><span>I\'m Feeling Creative</span></a><a href="http://www.paperwhite-studio.com/studio/" target="_blank"><span>I\'m Feeling Good</span></a><a href="http://www.paperwhite-studio.com/studio/" target="_blank"><span>I\'m Feeling Techy</span></a><a href="http://www.paperwhite-studio.com/news/" target="_blank"><span>I\'m Feeling Intrigued</span></a><a href="http://www.paperwhite-studio.com/contact/" target="_blank"><span>I\'m Feeling Social</span></a></div></div>' ).insertAfter( ".front #search-api-page-search-form-home .form-submit" );
 
-    /* duplicate the search button on the front page */
-    
-
-/* START: SMARTMENUS */
-
-    // initialise smartmenus for main menu
-    // $('#main-menu').smartmenus();
-
-/* END: SMARTMENUS */
-
-/* START: ISOTOPE */
-
-    // add btn class to isotope filter lists for all views
-    $(document).ready(function() {
-        // $('ul.isotope-filters li>a').addClass('isotope-button'); // add a class to the button links
-
-/* BEGIN: Feeling Lucky javascript */
-    if (!Modernizr.touchevents) { 
-        $( '<input type="submit" id="edit-submit-2" name="op" value="I\'m feeling lucky!" class="btn-primary btn form-submit"><div class="shuffler" role="button"><div class="pattern"><a href="http://www.paperwhite-studio.com/work/" target="_blank"><span>I\'m Feeling Creative</span></a><a href="http://www.paperwhite-studio.com/studio/" target="_blank"><span>I\'m Feeling Good</span></a><a href="http://www.paperwhite-studio.com/studio/" target="_blank"><span>I\'m Feeling Techy</span></a><a href="http://www.paperwhite-studio.com/news/" target="_blank"><span>I\'m Feeling Intrigued</span></a><a href="http://www.paperwhite-studio.com/contact/" target="_blank"><span>I\'m Feeling Social</span></a></div></div>' ).insertAfter( ".front #search-api-page-search-form-home .form-submit" );
-
-         var button_offset = $('#edit-submit-2').offset();
-         var left = button_offset.left;
-         var top = button_offset.top;
+            var button_offset = $('#edit-submit-2').offset();
+            var left = button_offset.left;
+            var top = button_offset.top;
          
-         $('.shuffler').offset({ top: top, left: left });
+            $('.shuffler').offset({ top: top, left: left });
          
-         $('.shuffler').hover(function() {
-           $('.shuffler').css('opacity', '1');
-           do {
-             var random = Math.floor(Math.random() * 5 - 3);
-           } while(random == 0);
-           console.log(random);
-           console.log(-random * 34);
-           $('.shuffler .pattern').animate({
-             top: -102 + -random * 34,
-           }, 400, function() {
-             // Animation complete.
-           });
-         }, function() {
-           $('.shuffler').css('opacity', '0');
-         });
-     }
-     else {
-        $( '<input type="submit" id="edit-submit-2" name="op" value="I\'m feeling lucky!" class="btn-primary btn form-submit">').insertAfter( ".front #search-api-page-search-form-home .form-submit" );
-     }
-         
+            $('.shuffler').hover(function() {
+            $('.shuffler').css('opacity', '1');
+                do {
+                var random = Math.floor(Math.random() * 5 - 3);
+                } while(random == 0);
+                console.log(random);
+                console.log(-random * 34);
+                $('.shuffler .pattern').animate({
+                top: -102 + -random * 34,
+                }, 400, function() {
+                // Animation complete.
+                });  
+            }, function() {
+            $('.shuffler').css('opacity', '0');
+            });
+        }
+        else {
+            $( '<input type="submit" id="edit-submit-2" name="op" value="I\'m feeling lucky!" class="btn-primary btn form-submit">').insertAfter( ".front #search-api-page-search-form-home .form-submit" );
+        }
+    }); 
 /* END: Feeling Lucyk javascript */
+    
+    $(document).ready(function() {
 
         // modal window for showcasing the icons
         //$('#icons').modal(options);
